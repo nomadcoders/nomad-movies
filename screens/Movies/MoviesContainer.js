e2c30a85c9688f47e49047998854fe4616a5fd48
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-12-11 15:54:40 +0900

    #4.3 Refactor Home with Advanced ES6

--- screens/Movies/MoviesContainer.js
+++ screens/Movies/MoviesContainer.js
@@ -12,20 +12,32 @@ export default class extends React.Component {
   };
 
   async componentDidMount() {
+    let upcoming, popular, nowPlaying, error;
     try {
-      const upcoming = await movies.getUpcoming();
-      const popular = await movies.getPopular();
-      const nowPlaying = await movies.getNowPlaying();
-      console.log(upcoming, popular, nowPlaying);
+      ({
+        data: { results: upcoming }
+      } = await movies.getUpcoming());
+      ({
+        data: { results: popular }
+      } = await movies.getPopular());
+      ({
+        data: { results: nowPlaying }
+      } = await movies.getNowPlaying());
     } catch {
-      this.setState({ error: "Can't get Movies." });
+      error = "Can't get Movies.";
     } finally {
-      this.setState({ loading: false });
+      this.setState({
+        loading: false,
+        error,
+        upcoming,
+        popular,
+        nowPlaying
+      });
     }
   }
 
   render() {
-    const { loading } = this.state;
+    const { loading, upcoming, popular, nowPlaying } = this.state;
     return <MoviesPresenter loading={loading} />;
   }
 }
