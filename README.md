86a77ca13f525d06657fd3af389a49233a223d58
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-12-11 12:39:14 +0900

    #1.3 Constants and Screens Set Up

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Nomad Movies
 
 iOS / Android Movie Discovery App built with React Native.
+
+## Screens
+
+- [ ] Movies
+- [ ] TV Shows
+- [ ] Search
+- [ ] Detail
---
+++ constants/Colors.js
@@ -0,0 +1,5 @@
+export const BG_COLOR = "black";
+export const WHITE = "white";
+export const GREY_COLOR = "#bdc3c7";
+export const INACTIVE_COLOR = "#7f8c8d";
+export const ACTIVE_COLOR = WHITE;
